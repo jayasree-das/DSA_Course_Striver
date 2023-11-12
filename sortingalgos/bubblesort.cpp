8f77c5beb0872e99012edb095dfb31f1e215b23f
purnendu901 <94794846+jayasree-das@users.noreply.github.com>
2023-11-12 10:05:58 +0530

    added optimal bubblesort

--- sortingalgos/bubblesort.cpp
+++ sortingalgos/bubblesort.cpp
@@ -1,16 +1,23 @@
 /*T.C:- O(n^2) for the worst and average cases
+T.C:- O(n) for the best cases when the array is already sorted
 S.C:- O(1)*/
 #include<bits/stdc++.h>
 using namespace std;
 
 void bubble_sort(int arr[],int n){
     for(int i=n-1;i>=0;i--){
+        int didswap=0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=temp;
+                didswap=1;//if swapping occurs that is the array is unsorted
             }
+        }
+        /*if in the first iteration only we see that there was no swapping it means the array is already sorted so we break out from the loop*/
+        if(didswap==0){
+            break;
         }
     }
     cout<< "After Bubble Sort : "<< '\n';
